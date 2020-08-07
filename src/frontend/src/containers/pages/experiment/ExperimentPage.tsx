1ccb709dde2f12837bf81831de209660a8582208
yk2030yk <yk.ngsyk@gmail.com>
2020-08-08 06:04:56 +0900

    fix; remove unused modules

--- src/frontend/src/containers/pages/experiment/ExperimentPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import ExperimentPage from '@/components/pages/experiment/ExperimentPage'
-
-const PageContainer: React.FC = () => {
-  return <ExperimentPage />
-}
-
-export default PageContainer
