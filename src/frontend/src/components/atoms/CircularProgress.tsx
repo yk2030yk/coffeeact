bf022712ddbad85c2aec1dbbd0ec81d4978ddc66
yk2030yk <yk.ngsyk@gmail.com>
2020-08-08 15:03:13 +0900

    fix: add loader props types

--- src/frontend/src/components/atoms/CircularProgress.tsx
+++ src/frontend/src/components/atoms/CircularProgress.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import PropagateLoader from 'react-spinners/PropagateLoader'
+
 import { theme } from '@/styles'
 
-const CustomClipLoader: React.FC = () => (
-  <PropagateLoader color={theme.color.reactBlue} />
+type PropagateLoaderProps = React.ComponentProps<typeof PropagateLoader>
+
+type Props = PropagateLoaderProps
+
+const CustomClipLoader: React.FC<Props> = (props) => (
+  <PropagateLoader color={theme.color.reactBlue} {...props} />
 )
 
 export default CustomClipLoader
