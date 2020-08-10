2173bc953174a85549364e79d78794203544aee7
yk2030yk <yk.ngsyk@gmail.com>
2020-08-11 04:12:38 +0900

    fix: can not import formatPath

--- src/frontend/src/components/molecules/footer/LinkList/index.tsx
+++ src/frontend/src/components/molecules/footer/LinkList/index.tsx
@@ -2,26 +2,22 @@ import React from 'react'
 
 import * as S from './index.styled'
 import { FormattedMessage } from '@/components/atoms'
-import { formatPath as formatHomePagePath } from '@/containers/routes/pageRoutes/HomePageRoute'
-import { formatPath as formatAboutPagePath } from '@/containers/routes/pageRoutes/AboutPageRoute'
-import { formatPath as formatProfilePagePath } from '@/containers/routes/pageRoutes/ProfilePageRoute'
-import { formatPath as formatLoginPagePath } from '@/containers/routes/pageRoutes/LoginPageRoute'
 
 const links = [
   {
-    path: formatHomePagePath(),
+    path: '/',
     id: 'app.page.home',
   },
   {
-    path: formatAboutPagePath(),
+    path: '/about',
     id: 'app.page.about',
   },
   {
-    path: formatProfilePagePath(),
+    path: '/profile',
     id: 'app.page.profile',
   },
   {
-    path: formatLoginPagePath(),
+    path: '/login',
     id: 'app.page.admin.login',
   },
 ]
