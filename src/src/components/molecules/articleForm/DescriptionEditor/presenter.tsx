f2ffe4c3a443e22b9b8a53ee9a89b242d144d01d
yk2030yk <yk.ngsyk@gmail.com>
2020-08-20 19:54:44 +0900

    fix

--- src/src/components/molecules/articleForm/DescriptionEditor/presenter.tsx
+++ src/src/components/molecules/articleForm/DescriptionEditor/presenter.tsx
@@ -3,14 +3,13 @@ import React from 'react'
 import * as S from './styled'
 
 import { Editor } from '@tinymce/tinymce-react'
+import { TINYMCE_API_KEY } from '@/tinymceConfig'
 
 type Props = {
   defaultValue: string
   description: string
   handleEditorChange: (content: string) => void
 }
-
-const TINYMCE_API_KEY = 'zfnd2ibeiddh09exp4axlbd3tu6mu1bxt1jnmm3h12n962bx'
 
 export const DescriptionEditorPresenter: React.FC<Props> = ({
   defaultValue,
